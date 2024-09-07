e3b25cb5d49b5748da945d8bd43870f4efa49203
Juni <ikjunim@gmail.com>
2024-09-07 14:36:01 +1000

    ok safari is that enough? you just happen to not make everything 3d preserve or whatever it is just what the fuck is wrong iwth you????? it take me like 2 days to figure out i needed this extra line? wasting everyone's time

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import PageManager from './Page/PageManager';
-// javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
+
 function App() {
   return <PageManager/>
 }
